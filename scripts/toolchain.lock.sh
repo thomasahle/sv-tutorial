c65ed7bf7ee58e1666720c9b0c5b7b116561b025
Thomas Dybdahl Ahle <thomas@ahle.dk>
2026-02-25 16:44:44 +0100

    Update pinned Surfer artifact SHA256

--- scripts/toolchain.lock.sh
+++ scripts/toolchain.lock.sh
@@ -11,4 +11,4 @@ readonly CIRCT_REF_LOCKED="6334424eee194b21b9b33148742071e8201fbbc2"
 readonly CIRCT_LLVM_SUBMODULE_REF_LOCKED="972cd847efb20661ea7ee8982dd19730aa040c75"
 
 readonly SURFER_ARTIFACT_URL_LOCKED="https://gitlab.com/surfer-project/surfer/-/jobs/artifacts/main/download?job=pages_build"
-readonly SURFER_ARTIFACT_SHA256_LOCKED="a8eba2eba26d6a2bc34dd6c347b65ebbc10acbfbb31769f2bf091f4f97f01814"
+readonly SURFER_ARTIFACT_SHA256_LOCKED="2a684122436e7a7729cc4e57062fdc2ce8ec5fa096d84ca383dd59011012b873"
